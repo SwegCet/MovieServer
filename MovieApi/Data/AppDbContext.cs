8923c2d1598da940587ab5e7255476e9b8f5b915
SwegCet <ethantruong0804@gmail.com>
2025-12-14 21:10:34 -0800

    Grab data set from supabase table

--- MovieApi/Data/AppDbContext.cs
+++ MovieApi/Data/AppDbContext.cs
@@ -1,41 +1,31 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MovieApi.Models;
-using System;
 
-namespace MovieApi.Data
+namespace MovieApi.Data;
+
+public class AppDbContext : DbContext
 {
-    public class AppDbContext : DbContext
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Profile> Profiles => Set<Profile>();
+    public DbSet<FavoriteMovie> FavoriteMovies => Set<FavoriteMovie>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        modelBuilder.Entity<Profile>().ToTable("profiles", "public");
+        modelBuilder.Entity<FavoriteMovie>().ToTable("favorite_movies", "public");
 
-        public DbSet<Profile> Profiles { get; set; } 
-        public DbSet<FavoriteMovies> FavoriteMovies { get; set; }
+        modelBuilder.Entity<Profile>().HasIndex(p => p.Username).IsUnique();
+        modelBuilder.Entity<Profile>().HasIndex(p => p.Email).IsUnique();
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Profile>(entity =>
-            {
-                entity.ToTable("profiletest");
-                entity.HasKey(p => p.Id);
-                entity.Property(p => p.Id).HasColumnName("id");
-                entity.Property(p => p.Username).HasColumnName("username");
-                entity.Property(p => p.CreatedAt).HasColumnName("created_at");
-            });
+        modelBuilder.Entity<FavoriteMovie>()
+            .HasIndex(f => new { f.ProfileId, f.TmdbMovieId })
+            .IsUnique();
 
-            modelBuilder.Entity<FavoriteMovies>(entity =>
-            {
-                entity.ToTable("favoritetest");
-                entity.HasKey(f => f.Id);
-                entity.Property(f => f.UserId).HasColumnName("id");
-                entity.Property(f => f.Id).HasColumnName("user_id");
-                entity.Property(f => f.MovieApiId).HasColumnName("movie_api_id");
-                entity.Property(f => f.CreatedAt).HasColumnName("created_at");
-
-                entity.HasOne(f => f.User)
-                    .WithMany().
-                    HasForeignKey(f => f.User.Id);
-            });
-        }
-
+        modelBuilder.Entity<Profile>()
+            .HasMany(p => p.FavoriteMovies)
+            .WithOne(f => f.Profile!)
+            .HasForeignKey(f => f.ProfileId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
