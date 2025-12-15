d02091c14f1fb5b9b493096a015945caf791a702
SwegCet <ethantruong0804@gmail.com>
2025-12-14 21:09:05 -0800

    Match DB columns for profile table in Supabase

--- MovieApi/Models/Profile.cs
+++ MovieApi/Models/Profile.cs
@@ -1,13 +1,33 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace MovieApi.Models
+namespace MovieApi.Models;
+
+[Table("profiles", Schema = "public")]
+public class Profile
 {
-    public class Profile
-    {
-        public Guid Id { get; set; } // uuid
-        public required string Username { get; set; }
-        public DateTime CreatedAt { get; set; }
+    [Key]
+    [Column("id")]
+    public Guid Id { get; set; } = Guid.NewGuid();
 
-        public ICollection<FavoriteMovies> FavoriteMovies { get; set; } = new List<FavoriteMovies>();
-    }
+    [Required, MaxLength(32)]
+    [Column("username")]
+    public string Username { get; set; } = "";
+
+    [Required, MaxLength(255)]
+    [Column("email")]
+    public string Email { get; set; } = "";
+
+    [Required]
+    [Column("password_hash")]
+    public string PasswordHash { get; set; } = "";
+
+    [Required]
+    [Column("role")]
+    public string Role { get; set; } = "User";
+
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public List<FavoriteMovie> FavoriteMovies { get; set; } = new();
 }
